f8f11dc204cf25828a9af2dc44170b728dee1b60
aj200288 <aj2002@student.uni-lj.si>
2025-12-01 10:00:10 +0100

    completed AoC25 day01 - unoptimised

--- 2025/day01/main.cpp
+++ 2025/day01/main.cpp
@@ -6,12 +6,48 @@
 
 
 int main() {
+    int num = 50;
+    int sum = 0; 
+
+
     std::ifstream file("input.txt");
     std::string line{};
     while (std::getline(file, line)) {
-        
+        int add = std::stoi(line.substr(1));
+
+        if (line[0] == 'L') {
+            if (num == 0)
+                --sum;
+
+            for (int i = 0; i < add; ++i){
+                --num;
+               
+                if (num < 0) {
+                    num = 99;
+                    ++sum;
+                }
+            }
+
+            if (num == 0)
+                ++sum;
+        }
+        else {
+            for (int j = 0; j < add; ++j) {
+                ++num;
+
+                if (num > 99) {
+                    num = 0;
+                    ++sum;
+                }
+            }
+        }
+
+        std::cout << num << ": " << sum << std::endl;
+
+
     }
     
+    std::cout << sum << std::endl;
     
     return 0;
 }
