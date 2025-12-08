de29acb504b74cfb7f1ef26145f9e0e95f52be63
aj200288 <aj2002@student.uni-lj.si>
2025-12-08 21:22:21 +0100

    berfore code improvements

--- 2025/day08/functions.h
+++ 2025/day08/functions.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cmath>
 
+
 long long distance(std::vector<long long> vec1, std::vector<long long> vec2);
 int is_present(size_t p, const std::vector<std::vector<size_t>> &groups);
 
